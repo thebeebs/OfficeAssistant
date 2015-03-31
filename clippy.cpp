011beb5619956808e6e605408f4e140f4c8dd2fe
thebeebs <martinbeeby@hotmail.com>
2015-03-31 23:04:03 +0100

    Adding Base Libraries

---
+++ clippy.cpp
@@ -0,0 +1,11 @@
+// clippy.cpp : main project file.
+
+#include "stdafx.h"
+
+using namespace System;
+
+int main(array<System::String ^> ^args)
+{
+    Console::WriteLine(L"Hello April 1st");
+    return 0;
+}
